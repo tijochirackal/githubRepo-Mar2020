73784fbdc2200f6995d8de0a35f9616b0728ff45
tijochirackal <tijochirackaltom@gmail.com>
2020-06-06 13:37:01 +0100

    addaed test 2

--- src/uk/axone/devintest/github/UnitTests.java
+++ src/uk/axone/devintest/github/UnitTests.java
@@ -9,4 +9,8 @@ public class UnitTests {
     public void test1(){
         System.out.println("Hello - running test 1");
     }
+    @Test
+    public void test2(){
+        System.out.println("new");
+    }
 }
