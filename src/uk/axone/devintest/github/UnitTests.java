777a6336b47f8b2cdd763510422430b215542207
tijochirackal <tijochirackaltom@gmail.com>
2020-06-06 09:37:56 +0100

    committing my project first time

---
+++ src/uk/axone/devintest/github/UnitTests.java
@@ -0,0 +1,12 @@
+package uk.axone.devintest.github;
+
+import org.junit.Test;
+
+public class UnitTests {
+
+
+    @Test
+    public void test1(){
+        System.out.println("Hello - running test 1");
+    }
+}
